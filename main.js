9d60893a90c670e2ff0d12955504e1fddedb056e
thsxogus <sth122603@gmail.com>
2021-12-08 23:25:13 +0900

    js

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
-const player = {
-    name:"nico",
-    sayHello: function(otherPersonName){
-        console.log("hello " + otherPersonName + " nice to meet you ");
-    },  
-};
+const calculator = {
+    add: function(a, b) {
+        console.log(a, b);
+    },
+    minus:function(a,b){
+        console.log(a - b);
+    },
+    divide:function(a, b){
+        console.log(a / b);
+    },
+    multi:function(a, b){
+        console.log(a * b);
+    },
+    powerOf:function(a, b){
+        console.log(a ** b);
+    }
+}
 
-player.sayHello("lynn");
-player.sayHello("nico");
+calculator.add(7,8);
+calculator.minus(10,8);
+calculator.divide(16,8);
+calculator.multi(7,8);
+calculator.powerOf(7,8);
