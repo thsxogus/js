5fdadc1d81391733f11367babffa4bee602b3126
thsxogus <sth122603@gmail.com>
2021-12-09 18:36:22 +0900

    js

--- main.js
+++ main.js
@@ -1,23 +1,3 @@
-const calculator = {
-    add: function(a, b) {
-        console.log(a, b);
-    },
-    minus:function(a,b){
-        console.log(a - b);
-    },
-    divide:function(a, b){
-        console.log(a / b);
-    },
-    multi:function(a, b){
-        console.log(a * b);
-    },
-    powerOf:function(a, b){
-        console.log(a ** b);
-    }
-}
+const age = parseInt(prompt("How old are you?"));
 
-calculator.add(7,8);
-calculator.minus(10,8);
-calculator.divide(16,8);
-calculator.multi(7,8);
-calculator.powerOf(7,8);
+console.log(age);
