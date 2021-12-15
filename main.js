9c7ed65ef01dcf45bb6eaf0c23ede838a2ede572
thsxogus <sth122603@gmail.com>
2021-12-15 18:40:32 +0900

    js

--- main.js
+++ main.js
@@ -1,38 +1,12 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-    h1.style.color = "blue";
-}
-
-function handleMouseEnter(){
-    h1.innerText = "Mouse is here";
-}
-
-function handleMouseLeave(){
-    h1.innerText = "Mouse is gone";
-}
-
-function handlewindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy(){
-    alert("copier!");
-}
-
-function handlewindowOffline(){
-    alert("SOS no WIFI");
-}
-
-function handleWindowOnline(){
-    alert("ALL GOOD");
+    const clickedClass = "clicked"
+   if (h1.className === "clickedClass") {
+       h1.className = "";
+   } else {
+       h1.className = "clickedClass";
+   }
 }
 
 h1.addEventListener("click",handleTitleClick );
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handlewindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handlewindowOffline);
-window.addEventListener("online", handleWindowOnline);
