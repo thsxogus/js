e66b67b1aa778fd171ddb1138ab290c5aca9760d
thsxogus <sth122603@gmail.com>
2021-12-15 17:31:58 +0900

    js

--- main.js
+++ main.js
@@ -1,17 +1,38 @@
-const title = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-    title.style.color = "blue";
+    h1.style.color = "blue";
 }
 
 function handleMouseEnter(){
-    title.innerText = "Mouse is here";
+    h1.innerText = "Mouse is here";
 }
 
 function handleMouseLeave(){
-    title.innerText = "Mouse is gone";
+    h1.innerText = "Mouse is gone";
 }
 
-title.addEventListener("click",handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
+function handlewindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy(){
+    alert("copier!");
+}
+
+function handlewindowOffline(){
+    alert("SOS no WIFI");
+}
+
+function handleWindowOnline(){
+    alert("ALL GOOD");
+}
+
+h1.addEventListener("click",handleTitleClick );
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handlewindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handlewindowOffline);
+window.addEventListener("online", handleWindowOnline);
