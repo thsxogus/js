451fc1de9c8164dba11b883befaaa162829eb1dd
thsxogus <sth122603@gmail.com>
2021-12-23 17:33:23 +0900

    js

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-const loginform = document.querySelector("#login-form");
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
-    event.preventDefault();
-    console.log(loginInput.value);
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-
-loginform.addEventListener("submit", onLoginSubmit);
+loginForm.addEventListener("submit", onLoginSubmit);
